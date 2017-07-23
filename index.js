1bfc599e337e864f5a0e94944f7c05cc43335f8d
Tianqi Liu <tianqi.liu@mail.utoronto.ca>
2017-07-23 00:37:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var crawler = require('../UofT-TimeTableCrawler/crawler');
+var results = crawler.crawl();
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        results.then(function(timetablesJSON) {
+            console.log('waiting for crawler');
+            socket.emit('chat message', timetablesJSON);
+        });
+    });
+});
+
+server.listen(3000, function() {
+    console.log('listening on port 3000...');
+});
